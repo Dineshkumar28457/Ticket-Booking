ae6562cda06aebca799f85cbad80ddc540cf5471
Dinesh <107694812+Dineshkumar28457@users.noreply.github.com>
2022-09-06 10:22:06 +0530

    Delete Ticketbooking-main directory

--- Ticketbooking-main/Ticket_Booking_System/src/main/java/com/Ticketbooking/Repository/AdminformRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Ticketbooking.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.Ticketbooking.model.*;
-
-@Repository
-public interface AdminformRepo extends JpaRepository<Adminformmodel, Long>{
-                                              
-}
